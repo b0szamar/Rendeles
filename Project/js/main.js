5d94fbad31ab4c99cfd71d8f3132ec8d1b03e86c
2020b-Szabó Marcell <74899044+b0szamar@users.noreply.github.com>
2023-10-02 20:01:23 +0200

    Add files via upload

---
+++ Project/js/main.js
@@ -0,0 +1,36 @@
+function Light(){
+    document.querySelectorAll('.cim').forEach((item) => {
+        item.style.color = 'black';
+        item.style.backgroundColor='white'
+      })
+    document.querySelectorAll('.hetkoz').forEach((item1) => {
+        item1.style.color = 'black';
+        item1.style.backgroundColor='white'
+      })
+    document.querySelectorAll('.prog').forEach((item2) => {
+        item2.style.color = 'black';
+        item2.style.backgroundColor='white'
+      })
+    document.querySelectorAll('.hetveg').forEach((item3) => {
+        item3.style.color = 'black';
+        item3.style.backgroundColor='white'
+      })
+}
+function Dark(){
+    document.querySelectorAll('.cim').forEach((item) => {
+        item.style.color = 'white';
+        item.style.backgroundColor='black'
+      })
+    document.querySelectorAll('.hetkoz').forEach((item1) => {
+        item1.style.color = 'white';
+        item1.style.backgroundColor='black'
+      })
+    document.querySelectorAll('.prog').forEach((item2) => {
+        item2.style.color = 'white';
+        item2.style.backgroundColor='black'
+      })
+    document.querySelectorAll('.hetveg').forEach((item3) => {
+        item3.style.color = 'white';
+        item3.style.backgroundColor='black'
+      })
+}
